2c6e259f0e993e114567e417048f82c1efcb391a
apit637cnr <apit637cnr@gmail.com>
2024-03-26 22:36:37 +0700

    add compponent header

---
+++ react-movie/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Blog</li>
+                <li>Contact Us</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
